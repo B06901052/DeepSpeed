dc78dbae6a4c8f1aca8df670209aece7a3316b42
Joseph Feng <b06901052@ntu.edu.tw>
2022-07-29 18:49:54 +0800

    bugfix: including params which requires_grad=False

--- flops_profiler/profiler.py
+++ flops_profiler/profiler.py
@@ -326,8 +326,7 @@ class FlopsProfiler(object):
         def add_or_reset_attrs(module):
             module.__flops__ = 0
             module.__macs__ = 0
-            module.__params__ = sum(p.numel() for p in module.parameters()
-                                    if p.requires_grad)
+            module.__params__ = sum(p.numel() for p in module.parameters())
             module.__start_time__ = 0
             module.__duration__ = 0
 
